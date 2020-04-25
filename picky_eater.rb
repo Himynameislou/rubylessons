c0395ca3c10b72cc99cc80332ebc5baadb174c3f
Himynameislou <louie1598@gmail.com>
2020-04-25 06:19:38 +0000

    End of 32; do coding Activity

--- picky_eater.rb
+++ picky_eater.rb
@@ -1,32 +1,43 @@
-# foods = ["Candy", "Soda", "Lettuce", "McDonalds", "KFC", "Mangos", "Carrots", "Onions"]
-# vegetables = ["Lettuce", "Broccoli", "Carrots", "Onions"]
+# foods = ["Candy", "Soda", "Lettuce", "McDonalds", "KFC", "Mango", "Carrot"]
+# vegetables = ["Lettuce", "Broccoli", "Carrot", "Onion"]
+# fruits = ["Apple", "Orange", "Mango", "Pineapple"]
 
 # foods.each do |food|
 #   if vegetables.include?(food)
 #     4.times do
 #       puts "Gross, I hate #{food}"
 #     end
+#   elsif fruits.include?(food)
+#     puts "How about we a make a smoothie with #{food}"
 #   else
-#     puts "Om nom nom ... I love #{food}"
+#     puts "Om nom nom... I love #{food}"
 #   end
 # end
 
+# puts "Enter a food to feed me:"
+# user_food = gets.chomp
+# puts "Om nom nom... I love #{user_food}"
 
-
-
-
-foods = ["Candy", "Soda", "Lettuce", "McDonalds", "KFC", "Mangos", "Carrots", "Onions"]
-vegetables = ["Lettuce", "Broccoli", "Carrots", "Onions"]
-fruits = ["Apple", "Oranges", "Mangos", "Pineapple"]
-
-foods.each do |food|
+def kid_eat(food)
+  vegetables = ["Lettuce", "Broccoli", "Carrot", "Onion"]
+  fruits = ["Apple", "Orange", "Mango", "Pineapple"]
   if vegetables.include?(food)
     4.times do
       puts "Gross, I hate #{food}"
     end
   elsif fruits.include?(food)
-    puts "How about we make a smoothie with #{food}"
+    puts "How about we a make a smoothie with #{food}"
   else
-    puts "Om nom nom ... I love #{food}"
+    puts "Om nom nom... I love #{food}"
   end
 end
+
+foods = ["Candy", "Soda", "Lettuce", "McDonalds", "KFC", "Mango", "Carrot"]
+
+foods.each do |food|
+  kid_eat(food)
+end
+
+puts "Enter a food to feed me:"
+user_food = gets.chomp
+kid_eat(user_food)
